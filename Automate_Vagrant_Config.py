c95be7e233559c8d2d4dac282e14690f585e2081
onebrownsound <dmodica2@pride.hofstra.edu>
2015-06-15 15:52:36 -0400

    Added mUserOsChoice

--- Automate_Vagrant_Config.py
+++ Automate_Vagrant_Config.py
@@ -2,18 +2,21 @@
 OPERATING_SYSTEMS = {
 
     "Centos_6.5": {"name": "chef/centos-6.5",
+                   "description": "A standard CentOS 6.5 x64 base install",
                    "setting1": "value1",
                    "setting2": "value2",
                    "setting3": "value3",
                    "setting4": "value4"},
 
-    "Ubuntu_12_LTS": {"name": "hashicorp/precise64",
-                      "setting1": "value1",
-                      "setting2": "value2",
-                      "setting3": "value3",
-                      "setting4": "value4"},
+    "Ubuntu_12.04_LTS_64bit_Server": {"name": "hashicorp/precise64",
+                                      "description": "A standard CentOS 6.5 x64 base install",
+                                      "setting1": "value1",
+                                      "setting2": "value2",
+                                      "setting3": "value3",
+                                      "setting4": "value4"},
 
     "Lucid_32": {"name": "lucid32",
+                 "description": "TODO",
                  "setting1": "value1",
                  "setting2": "value2",
                  "setting3": "value3",
@@ -30,8 +33,12 @@ BASE_VAGRANT_CONFIG = """Vagrant.configure("2") do |config|
 
 end"""
 
+print "Greetings these are the follow choices for operatin systems ",OPERATING_SYSTEMS.keys()," Please select one (case matters)"
+#mUserOsChoice is a string which represents the users choice for operating system
+#is case sensitive and be aware for underscores
+mUserOsChoice=str(input())
+
 
 # The next line will open the Vagrant configuration as a file object known as f
 with open("Vagrantfile", "w+") as f:
     f.write(BASE_VAGRANT_CONFIG)
-
