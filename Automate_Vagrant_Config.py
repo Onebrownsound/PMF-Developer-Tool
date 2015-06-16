ddf10351c505b696ab16431ac914f9d530faaf7d
onebrownsound <dmodica2@pride.hofstra.edu>
2015-06-16 10:45:05 -0400

    Converted BASE_VAGRANT_CONFIG to a string.Template object. Which will allow substitutions based off user choice.

--- Automate_Vagrant_Config.py
+++ Automate_Vagrant_Config.py
@@ -1,3 +1,4 @@
+from string import Template
 # Global dictionary declaration, which houses all various configurations for each particular OS Choice
 OPERATING_SYSTEMS = {
 
@@ -23,32 +24,38 @@ OPERATING_SYSTEMS = {
                  "setting4": "value4"},
 }
 
-# This constant houses the baseline config file
-BASE_VAGRANT_CONFIG = """Vagrant.configure("2") do |config|
-  config.vm.box ="hashicorp/precise64"
+# This constant houses the baseline config file AS A TEMPLATE OBJECT
+# Template objects may not be treated as regular strings
+# In order to extract strings from a template object use the TEMPLATEOBJECT.safe_substitute() method,
+# Which will return the template object as a string with the appropriate substitutions
+BASE_VAGRANT_CONFIG = Template("""Vagrant.configure("2") do |config|
+  config.vm.box ="${operating_system}"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.provider "virtualbox" do |v|
     v.gui = true
   end
 
-end"""
+end""")
 
-#function responsible for prompting user for OS Choice
+# function responsible for prompting user for OS Choice
 def PromptUserChoice():
-    print "Greetings these are the follow choices for operatin systems ",OPERATING_SYSTEMS.keys()," Please select one (case matters)"
-    #mUserOsChoice is a string which represents the users choice for operating system
-    #is case sensitive and be aware for underscores
+    print "Greetings these are the follow choices for operatin systems ", OPERATING_SYSTEMS.keys(), " Please select one (case matters)"
+    # mUserOsChoice is a string which represents the users choice for operating system
+    # is case sensitive and be aware for underscores
     return raw_input()
 
-
-#Set mUserOsChoice to null, and repeat prompt until user response matches a key in OPERATING_SYSTEMS
-mUserOsChoice=None
+# Set mUserOsChoice to null, and repeat prompt until user response matches a key in OPERATING_SYSTEMS
+mUserOsChoice = None
 while (mUserOsChoice not in OPERATING_SYSTEMS.keys()):
-    mUserOsChoice=PromptUserChoice()
+    mUserOsChoice = PromptUserChoice()
+
+print(BASE_VAGRANT_CONFIG.safe_substitute(operating_system=OPERATING_SYSTEMS[mUserOsChoice]["name"]))
+
+
 
 
 
 
 # The next line will open the Vagrant configuration as a file object known as f
 with open("Vagrantfile", "w+") as f:
-    f.write(BASE_VAGRANT_CONFIG)
+    f.write(BASE_VAGRANT_CONFIG.safe_substitute(operating_system=OPERATING_SYSTEMS[mUserOsChoice]["name"]))
